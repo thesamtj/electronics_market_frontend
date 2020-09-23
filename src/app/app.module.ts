f8083c16aeb9515972fd8b9f070267efaa887ca6
Samuel Tijani <omalitijani@gmail.com>
2020-09-23 13:23:09 +0100

    Generate nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule,
