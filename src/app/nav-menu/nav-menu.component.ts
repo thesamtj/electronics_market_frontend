6d8918cc7ddd5d2a68231541fd5e2b3c549705ad
Samuel Tijani <omalitijani@gmail.com>
2020-09-23 18:40:17 +0100

    Implement nav logic

--- src/app/nav-menu/nav-menu.component.ts
+++ src/app/nav-menu/nav-menu.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from '../../../node_modules/rxjs';
+import { AccountService } from '../services/account.service';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.scss']
+  styleUrls: ['./nav-menu.component.scss'],
 })
 export class NavMenuComponent implements OnInit {
+  constructor(
+    private acct: AccountService,
+    private productservice: ProductService
+  ) {}
 
-  constructor() { }
+  LoginStatus$: Observable<boolean>;
 
-  ngOnInit(): void {
+  UserName$: Observable<string>;
+
+  ngOnInit() {
+    this.LoginStatus$ = this.acct.isLoggesIn;
+
+    this.UserName$ = this.acct.currentUserName;
   }
 
+  onLogout() {
+    this.productservice.clearCache();
+    this.acct.logout();
+  }
 }
