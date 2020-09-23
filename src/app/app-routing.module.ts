5e58cbb99259c8b15b6c74da79d513e967902ebd
Samuel Tijani <omalitijani@gmail.com>
2020-09-23 15:00:35 +0100

    Add complete routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', component: HomeComponent, pathMatch: 'full' },
+  {
+    path: 'products',
+    loadChildren: './products/products.module#ProductsModule',
+  },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'access-denied', component: AccessDeniedComponent },
   { path: '**', redirectTo: '/home' },
 ];
 
