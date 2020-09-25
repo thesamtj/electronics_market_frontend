b0b1c1467447a59cc6cd14bd30a8a57d625abd15
Samuel Tijani <omalitijani@gmail.com>
2020-09-25 15:00:24 +0100

    Implement access denied

--- src/app/errors/access-denied/access-denied.component.ts
+++ src/app/errors/access-denied/access-denied.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-access-denied',
   templateUrl: './access-denied.component.html',
-  styleUrls: ['./access-denied.component.scss']
+  styleUrls: ['./access-denied.component.scss'],
 })
 export class AccessDeniedComponent implements OnInit {
+  h2Message: string;
+  h1Message: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.h1Message = '403';
+    this.h2Message = 'Access Denied';
   }
-
 }
