725be66869a716bf4b98d3f866ad976f7f0e9e63
Samuel Tijani <omalitijani@gmail.com>
2020-09-28 10:54:30 +0100

    Implement logic

--- src/app/products/product-details/product-details.component.ts
+++ src/app/products/product-details/product-details.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../interfaces/product';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+  styleUrls: ['./product-details.component.scss'],
 })
 export class ProductDetailsComponent implements OnInit {
+  @Input() product: Product;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private productservice: ProductService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    let id = +this.route.snapshot.params['id'];
+
+    this.productservice
+      .getProductById(id)
+      .subscribe((result) => (this.product = result));
   }
-
 }
