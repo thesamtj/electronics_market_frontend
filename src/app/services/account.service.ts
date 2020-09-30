a5a159bdfe5cfe7b8143b7349ef7227021a1c08a
Samuel Tijani <omalitijani@gmail.com>
2020-09-30 17:16:32 +0100

    Remove unused declarations

--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, Subject, BehaviorSubject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import jwt_decode from "jwt-decode";
-import { decode } from 'punycode';
 
 @Injectable({
   providedIn: 'root',
